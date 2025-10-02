a37ab33effe834f03bcadb95ee8ca60620a2ece0
Shokri Francis Raoof <raoofs.de@gmail.com>
2025-10-02 17:02:22 +0200

    feat: Add animations package and implement fade-through transition for QuizScreen navigation

--- lib/src/features/learn/presentation/screens/learn_screen.dart
+++ lib/src/features/learn/presentation/screens/learn_screen.dart
@@ -1,18 +1,29 @@
 import 'package:clann/src/features/quiz/presentation/screens/drag_and_drop.dart';
 import 'package:clann/src/features/quiz/presentation/screens/quiz_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animations/animations.dart';
 
 class LearnScreen extends StatelessWidget {
   const LearnScreen({super.key});
+
+  Route _fadeThroughRoute(Widget page) => PageRouteBuilder(
+    pageBuilder: (_, __, ___) => page,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return FadeThroughTransition(
+        animation: animation,
+        secondaryAnimation: secondaryAnimation,
+        child: child,
+      );
+    },
+    transitionDuration: const Duration(milliseconds: 500),
+  );
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: OutlinedButton(
         onPressed: () {
-          Navigator.of(
-            context,
-          ).push(MaterialPageRoute(builder: (context) => QuizScreen()));
+          Navigator.of(context).push(_fadeThroughRoute(const QuizScreen()));
         },
         child: Text('Quiz'),
       ),
